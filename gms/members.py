cd956fb81e8add840f94b5fd2094b9fc58fcbf98
danborn-dominic <ddanborn@purdue.edu>
2023-12-07 16:51:32 -0500

    added edit and delete functionality

--- gms/members.py
+++ gms/members.py
@@ -36,3 +36,50 @@ def view():
     members = db.execute('SELECT * FROM members').fetchall()
     return render_template('register.html', members=members)
 
+@bp.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    db = get_db()
+    member = db.execute('SELECT * FROM Members WHERE member_id = ?', (id,)).fetchone()
+
+    if member is None:
+        flash('Member not found.')
+        return redirect(url_for('members.view'))
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone_number = request.form['phone']
+        dob = request.form['dob']
+        membership_type = request.form['type']
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+        elif not email:
+            error = 'Email is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            try:
+                db.execute(
+                    'UPDATE Members SET name = ?, email = ?, phone_number = ?, dob = ?, membership_type = ?'
+                    ' WHERE member_id = ?',
+                    (name, email, phone_number, dob, membership_type, id)
+                )
+                db.commit()
+                flash('Member updated successfully.')
+                return redirect(url_for('members.view'))
+            except db.IntegrityError as e:
+                flash(f'An error occurred: {e}.')
+                return redirect(url_for('members.edit', id=id))
+
+    return render_template('edit.html', member=member)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    db = get_db()
+    db.execute('DELETE FROM Members WHERE member_id = ?', (id,))
+    db.commit()
+    flash('Member successfully deleted.')
+    return redirect(url_for('members.view'))
